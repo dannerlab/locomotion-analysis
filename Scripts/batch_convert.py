cac4c809b79dfa268d6e14a8f3e083ebebb9ad57
Laura Schoenhals <98430273+LauraSchoenhals@users.noreply.github.com>
2024-08-13 14:49:09 -0400

    adapt for this repository and running multiple groups

--- Scripts/batch_convert.py
+++ Scripts/batch_convert.py
@@ -1,26 +1,26 @@
-#after splitting files, run this first to convert from smr to h5
-#run from GitHub directory containing both locoproc & mouse-V3-analysis
-#afterwards, it is recommended to manually move h5 files to an h5 only directory
-#should also move the phase files to the phase file directory
+""" after splitting smr files, run this first to convert to h5 """
 
 import os
 
-#directory = input("Enter the file path of the folder containing smr files you want to convert to h5: ")
-directory = 'mouse-V3-analysis/data/kinematics-modeling-v1/MUT_incline/smr'
-print(f'filepath: {directory}' )
-total_files = len(os.listdir(directory))
-exists = os.path.exists(directory)
+groups = ["V3Off_levelwalk", "V3Off_Levelwalk"]
+data_folder = "Sample_data"
+dirs = []
+for group in groups:
+    directory = f'locomotion-analysis/{data_folder}/{group}/smr'
+    dirs.append(directory)
 
-convert_to_h5 = "locoproc/script/convert_to_h5.py"
+convert_to_h5 = "locomotion-analysis/Scripts/convert_to_h5.py"
 
 processed_files = 0
-if os.path.exists(directory):
-    for file in os.listdir(directory):
-        f = os.path.join(directory, file)
-        os.system(f'python {convert_to_h5} -f {f} -u')   #runs convert_to_h5.py with u units on the files in the directory
-        processed_files += 1
 
-else:
-    print("the file path you entered does not exist")
+for directory in dirs:
+    if os.path.exists(directory):
+        for file in os.listdir(directory):
+            f = os.path.join(directory, file)
+            os.system(f'python {convert_to_h5} -f {f} -u')   #runs convert_to_h5.py with u units on the files in the directory
+            processed_files += 1
 
-print(f'processed files: {processed_files}/{total_files}')
+    else:
+        print("the file path you entered does not exist")
+
+print(f'processed files: {processed_files}')
