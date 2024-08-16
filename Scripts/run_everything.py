7919017ce2591c5ec754f692b887a29e751590d1
Laura Schoenhals <98430273+LauraSchoenhals@users.noreply.github.com>
2024-08-16 09:18:01 -0400

    implement

--- Scripts/run_everything.py
+++ Scripts/run_everything.py
@@ -1,6 +1,14 @@
+"""
+re-runs all scripts to generate:
+- step_table.csv with stepwise stats
+- animal_avg_&_stdv.csv with avgs and stdvs
+- ANOVA_results.csv with group comparisons of the stats in animal_avg_&_stdv.csv
+"""
 
-#step_table_initialize
+import os
 
-#animal_avgs.py
+os.system('python Scripts/step_table_initialize.py')
 
-#group_comparison.py
+os.system('python Scripts/animal_avgs.py')
+
+os.system('python Scripts/group_comparison.py')
