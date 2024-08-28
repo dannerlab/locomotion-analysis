b47c107bbad1b170419c70ded5202cd0c4415743
Laura Schoenhals <98430273+LauraSchoenhals@users.noreply.github.com>
2024-08-28 10:54:27 -0400

    Calculate and save ANOVA

--- Scripts/group_comparison.py
+++ Scripts/group_comparison.py
@@ -1,5 +1,6 @@
 """
 uses animal_avg & stdv to compare 2 groups via ANOVA
+needs filtering still
 """
 
 import pandas as pd
@@ -44,16 +45,69 @@ def compare_groups_means(animal_stats_fp):
 
     return results_df
 
-def compare_groups(step_table_fp):
-    step_table = pd.read_csv(step_table_fp)
-    step_table_grouped = step_table.groupby(['mouse-type', 'exp-type'])
-    selected_stats = get_numeric_col_names() #gets names of the columns that it makes sense to run these tests on
+def shapiro_and_k(standardized_residuals, alpha):
+    #tests for normalcy
+    shapiro_non_normal = {}
+    shapiro_normal = {}
+    k_non_normal = {}
+    k_normal = {}
 
+    for key in standardized_residuals:
+        shapiro_residual = shapiro(standardized_residuals[key])[1]
+        if shapiro_residual <= alpha: #add only non normal stats to the dictionary
+            shapiro_non_normal[key] = shapiro_residual
+        else:
+            shapiro_normal[key] = shapiro_residual
+
+        k_residual = stats.kstest(standardized_residuals[key], 'norm')[1]
+        if k_residual <= alpha: #adds only non normal stats to the dictionary
+            k_non_normal[key] = k_residual
+        else:
+            k_normal[key] = k_residual
+
+    #combined filtered
+    normal_stats = {}
+    non_normal_stats = {}
+    for key in standardized_residuals:
+        if key in shapiro_non_normal.keys():
+            if key in k_non_normal.keys():
+                non_normal_stats[key] = [shapiro_non_normal[key], k_non_normal[key]]
+            else:
+                non_normal_stats[key] = [shapiro_non_normal[key], k_normal[key]]
+        elif key in k_non_normal.keys():
+            non_normal_stats[key] = [shapiro_normal[key], k_non_normal[key]]
+        else:
+            normal_stats[key] = [shapiro_normal[key], k_normal[key]]
+
+    return normal_stats, non_normal_stats
+
+def calc_anova(step_table_grouped, selected_stats, alpha):
+    list_of_selected_groups = []
+    for group, data in step_table_grouped:
+        selected_data = data[selected_stats]
+        list_of_selected_groups.append(selected_data)
+
+    one_way_p = {}
+
+    for key in selected_stats:
+        group_stats = [] #list of selected columns (via key) for each group
+        for group in list_of_selected_groups:
+            group_stats.append(group[key])
+        p_val = f_oneway(*(group_stats))[1]
+        if p_val <= alpha: #filter for significance
+            one_way_p[key] = p_val
+
+    #format for nicer saving
+    one_way_p = pd.Series(one_way_p.values(),index=one_way_p.keys()).to_frame()
+    one_way_p = one_way_p.rename(columns = {0: 'ANOVA_p_value'})
+
+    return one_way_p
+
+def standardize_residuals(step_table_grouped, selected_stats):
     #combined residuals
     ##calculate residuals for each statistic and store in residuals dict
     residuals = {group: {} for group in step_table_grouped.groups.keys()}
     ##residuals dict is formatted as: {group: {stat: residuals col}}
-
     for group, data in step_table_grouped:
         selected_data = data[selected_stats]
         for col in selected_data:
@@ -61,7 +115,7 @@ def compare_groups(step_table_fp):
             col_residual = selected_data[col] - col_mean
             residuals[group][col] = list(col_residual)
 
-    combined_residuals = combine_dicts(*residuals.values())
+    combined_residuals = combine_dicts(*residuals.values()) #extracts so they are no longer by group
 
     #standardized residuals
     ##find stdv of residuals for all stats
@@ -75,46 +129,35 @@ def compare_groups(step_table_fp):
     for key in combined_residuals:
         standardized_residuals[key] = combined_residuals[key] / stdv_residuals[key]
 
-    #calculate normalcy
-    residuals_shapiro = pd.DataFrame()
-    residuals_k = pd.DataFrame()
-    for key in standardized_residuals:
-        residuals_shapiro[key] = shapiro(standardized_residuals[key])[1]
-        residuals_k[key] = stats.kstest(standardized_residuals[key], 'norm')[1]
+    return standardized_residuals
+
+def compare_groups(step_table_fp, alpha):
+    step_table = pd.read_csv(step_table_fp)
+    step_table_grouped = step_table.groupby(['mouse-type', 'exp-type'])
+    selected_stats = get_numeric_col_names() #gets names of the columns that it makes sense to run these tests on
+
+    standardized_residuals = standardize_residuals(step_table_grouped, selected_stats)
+
+    #calculate normalcy of residuals
+    ##This may not actually be what I want. Might be better to calculate normalcy of data itself
+    normal_stats, non_normal_stats = shapiro_and_k(standardized_residuals, alpha)
 
     #homogeneity of variance: one way p
-    list_of_selected_groups = []
-    for group, data in step_table_grouped:
-        selected_data = data[selected_stats]
-        list_of_selected_groups.append(selected_data)
+    one_way_p = calc_anova(step_table_grouped, selected_stats, alpha)
 
-    one_way_p = pd.DataFrame()
+    return one_way_p
 
-    for key in standardized_residuals:
-        one_way_p[key] = f_oneway(*(group for group in list_of_selected_groups))[1]
-
-
-    IPython.embed()
-    fjdkl
-
-
-    return
-
-def filter_significance(results_df, alpha=float):
-    filtered_results = results_df.where(results_df['C(animal_stats[independent_vars[0]])']['PR(>F)'] < alpha).dropna()
-    return filtered_results
 
 def main():
     animal_stats_fp = 'Sample_data/animal_avg_&_stdv.csv' #should use step_table.csv
     mean_results = compare_groups_means(animal_stats_fp)
     step_table_fp = 'Sample_data/step_table.csv'
-    compare_groups(step_table_fp)
+    alpha = 0.05
 
-    sig_results = filter_significance(results, alpha=0.05)
-
+    one_way_p = compare_groups(step_table_fp, alpha)
 
     save_name = os.path.join(os.path.split(animal_stats_fp)[0],'ANOVA_results.csv')
-    sig_results.to_csv(save_name)
+    one_way_p.to_csv(save_name)
     print(f'saved to: {save_name}')
 
     return
