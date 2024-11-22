c07a94e7647602ecde2b164a73eede8538237111
Laura Schoenhals <98430273+LauraSchoenhals@users.noreply.github.com>
2024-11-22 14:47:41 -0500

    now it works

--- Scripts/mouse_joint_angles_toe_touch.py
+++ Scripts/mouse_joint_angles_toe_touch.py
@@ -53,7 +53,6 @@ def get_max_toe_touch(trial_grouped):
 
 def get_stepwise_stats(mouse_id, mouse_data, stat):
     """calculates the average line for a group of trials"""
-    stats = get_continuous_stats()
  
     trial_grouped = mouse_data.groupby(['mouse-type', 'exp-type', 'mouse-id', 'trial-number'])
     max_stance_len, max_swing_len, stance_lens, swing_lens = get_max_lengths(trial_grouped)
@@ -62,17 +61,26 @@ def get_stepwise_stats(mouse_id, mouse_data, stat):
     
 
     steps_arr = np.empty((len(mouse_data), max_step_combined_len))
+    abs_step_i = 0
     for trial_i, (trial_id, trial_data) in enumerate(trial_grouped):
         h5_df = import_kinematics(trial_data['source-data-h5-path'].iloc[0])
+        #print(trial_data['source-data-h5-path'].iloc[0])
+        #print(h5_df.shape)
         h5_df['abs-idx'] = range(len(h5_df))
         steps_list = get_steps(trial_data, h5_df) #simply called steps in other scripts, here called steps_list to differentiate from steps_arr
+
         for step_i, step in enumerate(steps_list):
+            #print(f'step_i: {step_i}, trial_i: {trial_i}, abs_step_i: {abs_step_i}')
             step['toe-touch-adjusted-idx'] = step['abs-idx'] - step['abs-toe-touch-idx']
 
-            swing_zeros = np.nan*(np.ones(max_swing_len - swing_lens[trial_i][step_i])) #add one for toe touch
-            stance_zeros = np.nan*(np.ones(max_stance_len - stance_lens[trial_i][step_i]))
-            zeroed_step = list(swing_zeros) + list(step[stat]) + list(stance_zeros)
-            steps_arr[step_i] = zeroed_step
+            swing_pad = np.nan*(np.ones(max_swing_len - swing_lens[trial_i][step_i]))
+            stance_pad = np.nan*(np.ones(max_stance_len - stance_lens[trial_i][step_i]))
+            padded_step = list(swing_pad) + list(step[stat]) + list(stance_pad)
+
+            if len(padded_step) != steps_arr.shape[1]:
+                raise ValueError(f"Length of padded_step ({len(padded_step)}) does not match the number of columns in steps_arr ({steps_arr.shape[1]})")
+            steps_arr[abs_step_i] = padded_step 
+            abs_step_i += 1 #need to have this so that it doesn't overwrite previous steps instead of adding to them
             
             #printing statements for figuring out if we are zeroing correctly
             # print(f'max_stance_len: {max_stance_len}')
@@ -80,21 +88,23 @@ def get_stepwise_stats(mouse_id, mouse_data, stat):
             # print(f'max_toe_touch: {max_toe_touch}')
             # print(f'max_step_combined_len: {max_step_combined_len}') 
             # print(f'swing_len: {swing_lens[trial_i][step_i]}')
-            # print(f'swing_zeros: {len(swing_zeros)}')
+            # print(f'swing_pad: {len(swing_pad)}')
             # print(f'stance_len: {stance_lens[trial_i][step_i]}')
-            # print(f'stance_zeros: {len(stance_zeros)}')
+            # print(f'stance_pad: {len(stance_pad)}')
             # print(f'step_len: {len(step[stat])}')
-            # print(f'zeroed_step: {len(zeroed_step)}')
+            # print(f'padded_step: {len(padded_step)}')
             # print(f'toe_touch_step: {step['abs-toe-touch-idx'].iloc[0] - step['abs-idx'].iloc[0]}')
             # print()
-    avg_line = np.nanmean(steps_arr, axis=0)
-    stdv_line = np.nanstd(steps_arr, axis=0)
+ 
+    avg_line = np.mean(steps_arr, axis=0) 
+    stdv_line = np.std(steps_arr, axis=0)
+
         
     return steps_arr, avg_line, stdv_line, max_toe_touch, max_step_combined_len
 
 def graph_stats(mouse_id, mouse_data, steps_arr, avg_line, stdv_line, stat, max_toe_touch, max_length, main_dir, trial_colors = False):
     """graphs the average line for the mouse; also graphs individual steps for all trials; each trial is a color if you want"""
-    fig, ax = plt.subplots()
+
     #get colors for trial
     trial_grouped = mouse_data.groupby(['mouse-type', 'exp-type', 'mouse-id', 'trial-number'])
     if trial_colors:      
@@ -109,23 +119,28 @@ def graph_stats(mouse_id, mouse_data, steps_arr, avg_line, stdv_line, stat, max_
 
     #plot steps
     abs_step_i = 0
-    for trial_i, (trial_id, trial) in enumerate(trial_grouped):
-        for step_i, step in trial.iterrows():
-            if step_i == 0:
-                ax.plot(time_vec, steps_arr[abs_step_i], color= colors[trial_i], alpha=0.5, label= f'{trial_id[3]}')
-                abs_step_i += 1
+    labels = []
+    for trial_i, (trial_id, trial_data) in enumerate(trial_grouped):
+        for step_i, step in trial_data.iterrows():
+            label = f'trial_{trial_id[3]}'
+            if label not in labels:
+                labels.append(label)
+                plt.plot(time_vec, steps_arr[abs_step_i], color= colors[trial_i], alpha=0.5, label = label)
             else:
-                ax.plot(time_vec, steps_arr[abs_step_i], color= colors[trial_i], alpha=0.5)
-                abs_step_i += 1
+                plt.plot(time_vec, steps_arr[abs_step_i], color= colors[trial_i], alpha=0.5)
+            abs_step_i += 1
     
     #plot toe touch & avg step
-    ax.plot(avg_line, color='black', linewidth=2, label='average')
+    plt.plot(time_vec, avg_line, color='black', linewidth=2, label='average')
+    upper = avg_line + stdv_line
+    lower = avg_line - stdv_line
+    plt.fill_between(time_vec, lower, upper, color = 'black', alpha = 0.2)
     plt.axvline(time_vec[max_toe_touch], color = 'black', linestyle = '--')
     
     if trial_colors:
-        ax.legend()
-    ax.set_ylabel(stat)
-    ax.set_xlabel('time (ms)')
+        plt.legend()
+    plt.ylabel(stat)
+    plt.xlabel('time (ms)')
     mouse_group = '_'.join(mouse_id[0:2])
     save_folder = os.path.join(main_dir, 'angle_graphs', 'toe_touch_aligned', mouse_group)
     if not os.path.exists(save_folder):
@@ -142,14 +157,16 @@ def main(main_dir):
     step_table_unfiltered = pd.read_csv(os.path.join(main_dir, 'step_table.csv'))
     step_table, dropped_trials = exclude_trials(step_table_unfiltered)
     stats = get_continuous_stats()
+    rc_params = get_rc_params()
+    plt.rcParams.update(rc_params)
 
     step_table_grouped = step_table.groupby(['mouse-type', 'exp-type', 'mouse-id'])
     for stat in stats:
+        print(stat)
         for mouse_id, mouse_data in step_table_grouped:
-            mouse_steps_arr, mouse_avg_line, mouse_stdv, max_toe_touch, max_length = get_stepwise_stats(mouse_id, mouse_data, stat)
-            graph_stats(mouse_id, mouse_data, mouse_steps_arr, mouse_avg_line, mouse_stdv, stat, max_toe_touch, max_length, main_dir, True)
+                mouse_steps_arr, mouse_avg_line, mouse_stdv, max_toe_touch, max_length = get_stepwise_stats(mouse_id, mouse_data, stat)
+                graph_stats(mouse_id, mouse_data, mouse_steps_arr, mouse_avg_line, mouse_stdv, stat, max_toe_touch, max_length, main_dir, True)
     
-    #something
     return
 
 if __name__ == '__main__':
